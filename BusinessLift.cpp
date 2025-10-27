8e088c42878d3923ebf528440a5b398656b93506
George <77775699+TheSkyLand@users.noreply.github.com>
2025-10-27 15:01:28 +0300

    Update BusinessLift.cpp

--- BusinessLift.cpp
+++ BusinessLift.cpp
@@ -1,18 +1,56 @@
 #include <iostream>
+#include <cmath>
+using namespace std;
+
 //!WORK IN PROGRESS
 int main()
 {
 	int K = 2; //наибольшее количество людей в лифте
+
 	int	F = 3; //количество этажей
+
 	int a[] = { 3, 0, 1 };
+
 	// 3 на 1 этаже
 	// 0 на 2 этаже
 	// 1 на 3 этаже
-	int seconds{};
-	int result{};
 
-	for (int i = 0; i < std::size(a); i++)
+	int seconds{};
+
+
+
+	for (int i = 0; i <= F; i++)
 	{
-		std::cout << a[i];
+		seconds++;//добавь секунду за перемещение по этажам
+		if (i != F) {
+			
+			if (a[i] != 0)
+			{
+				continue;
+				if (a[i] >= K) //если людей в лифте больше чем большее количество, то добавь к секундам наибольшее количество людей в лифте
+				{
+					seconds += K;
+				}
+				else if(a[i] < K)//если людей в лифте меньше чем большее количество, то добавь к секундам то количество, сколько людей в лифте
+				{
+					seconds += a[i];
+				}
+			}
+			continue;
+		}
+		else {
+			break;
+		}
 	}
+	for (int i = F; i >= 0; i--)
+	{
+		seconds++;
+		if (i == 0)
+		{
+			break; 
+		}
+	}
+	
+    std::cout << seconds; //8
+	
 }
